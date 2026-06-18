c2204a734702ab27a7458ac6ff24868e162d56a9
Hknots <hknots99@gmail.com>
2026-06-18 15:15:25 +0200

    style: apply ktlint formatting to shared sources

--- fint-core-shared/src/main/kotlin/no/novari/core/shared/FintResourceExtension.kt
+++ fint-core-shared/src/main/kotlin/no/novari/core/shared/FintResourceExtension.kt
@@ -4,6 +4,8 @@ import no.novari.fint.model.FintIdentifikator
 import no.novari.fint.model.resource.FintResource
 
 fun FintResource.nonNullIdentifikators(): Map<String, FintIdentifikator> =
-    identifikators.orEmpty().mapNotNull { (field, identifikator) ->
-        identifikator?.let { field to it }
-    }.toMap()
+    identifikators
+        .orEmpty()
+        .mapNotNull { (field, identifikator) ->
+            identifikator?.let { field to it }
+        }.toMap()
