c71a9a827b7e573f3866a5582ae43d55736573e0
Hknots <hknots99@gmail.com>
2026-09-21 07:57:08 +0200

    feat: add ResourceStoreProperties to enabled configuration properties

--- fint-core-client-api/src/main/java/no/fintlabs/client/Application.java
+++ fint-core-client-api/src/main/java/no/fintlabs/client/Application.java
@@ -1,9 +1,11 @@
 package no.fintlabs.client;
 
 import no.fint.antlr.EnableFintFilter;
+import no.novari.core.shared.store.ResourceStoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ComponentScan(basePackages = {"no.fintlabs", "no.novari"})
 @ConfigurationPropertiesScan
+// TODO: Move away from ConfigurationPropertiesScan and use EnableConfigurationProperties instead
+@EnableConfigurationProperties(ResourceStoreProperties.class)
 public class Application {
 
     public static void main(String[] args) {
