c14b105f3e05bcc451b66107d2ab9b7641d18209
Hknots <hknots99@gmail.com>
2026-09-22 14:24:34 +0200

    feat: add timeout to OpaProperties

--- fint-core-client-api/src/main/java/no/fintlabs/client/security/opa/OpaProperties.kt
+++ fint-core-client-api/src/main/java/no/fintlabs/client/security/opa/OpaProperties.kt
@@ -1,10 +1,12 @@
 package no.fintlabs.client.security.opa
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import java.time.Duration
 
 /** When [enabled] is false, [OpaService] allows everything, so only the checks already in `SecurityConfiguration` apply. */
 @ConfigurationProperties("fint.security.opa")
 class OpaProperties {
     var enabled: Boolean = false
     var url: String = "http://fint-core-opa:8181"
+    var timeout: Duration = Duration.ofSeconds(2)
 }
